6a2a1d759da354f19018650c22e447b263efe83c
truonghoangduy <truonghoangduy03111999@gmail.com>
2020-12-04 12:53:19 +0700

    add Seeder Factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,6 +1,10 @@
 <?php
 
 namespace Database\Seeders;
+use App\Models\Cart;
+use App\Models\Product;
+use App\Models\Product_Cart;
+use App\Models\User;
 use Illuminate\Support\Facades\DB;
 
 use Illuminate\Database\Seeder;
@@ -12,14 +16,29 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
+
+
     public function run()
     {
-        for ($x = 0; $x <= 3; $x++) {
-            DB::table('products')->insert([
-                'name' => Str::random(10),
-                'description' => Str::random(10).'@gmail.com',
-                'price' => rand(10,100),
-            ]);        }
+        User::factory(10)->create();
+        Product::factory(20)->create();
+        Cart::factory(4)->create();
+        Product_Cart::factory(4)->create();
+//        for ($x = 0; $x <= 3; $x++) {
+//            DB::table('products')->insert([
+//                'name' => Str::random(10),
+//                'description' => Str::random(10).'@gmail.com',
+//                'price' => rand(10,100),
+//            ]);
+//            DB::table('users')->insert([
+//                'name' => Str::random(10),
+//                'email' => Str::random(10).'@gmail.com',
+//                'password' => Str::random(10),
+//                'role'=>[User::ADMIN,User::EDITOR,User::CUSTOMER][array_rand([User::ADMIN,User::EDITOR,User::CUSTOMER])],
+//            ]);
+//
+//        }
 //         \App\Models\User::factory(10)->create();
     }
 }
+
