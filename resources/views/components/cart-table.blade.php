b97a843a5064bb9d01730ca94d3c5eb48512d9ce
truonghoangduy <truonghoangduy03111999@gmail.com>
2020-12-05 16:51:27 +0700

    UI list out all the Cart created by user with CURD

---
+++ resources/views/components/cart-table.blade.php
@@ -0,0 +1,111 @@
+<div class="row">
+    <div class="col">
+        <div class="card">
+            <!-- Card header -->
+            <div class="card-header border-0">
+                <h3 class="mb-0">Cart</h3>
+            </div>
+            <!-- Light table -->
+            <div class="table-responsive">
+                <table class="table align-items-center table-flush">
+                    <thead class="thead-light">
+                    <tr>
+                        <th scope="col" class="sort" data-sort="name">id</th>
+
+                        <th scope="col" class="sort" data-sort="name">User</th>
+                        <th scope="col" class="sort" data-sort="status">Status</th>
+                        <th scope="col" class="sort" data-sort="completion">Order Date</th>
+                        <th scope="col"></th>
+                    </tr>
+                    </thead>
+                    <tbody class="list">
+                    @foreach($listOfCart as $cart)
+                        <tr>
+
+                            {{--                                                    Name - Picture--}}
+                            {{--                                                    Prices--}}
+                            {{--                                                    Status--}}
+                            <th scope="row">
+                                <div class="media align-items-center">
+                                    {{--                                    <a href="#" class="flex-row w mr-3">--}}
+                                    {{--                                        @if($product['pictureURL'] != null)--}}
+                                    {{--                                            <img alt="Image placeholder" class="thumbnail-img fit-image" src="{{\Illuminate\Support\Facades\URL::asset($product['pictureURL'])}}">--}}
+                                    {{--                                        @else--}}
+                                    {{--                                            <img alt="Image placeholder" src="../assets/img/theme/bootstrap.jpg">--}}
+                                    {{--                                        @endif--}}
+                                    {{--                                    </a>--}}
+                                    <div class="media-body">
+                                        <span class="name mb-0 text-sm">{{$cart['id']}}</span>
+                                    </div>
+                                </div>
+                            </th>
+                            <td class="budget">
+                                {{$cart->user_id}}
+                            </td>
+                            <td>
+                      <span class="badge badge-dot mr-4">
+                        <i class="bg-warning"></i>
+                        <span class="status">pending</span>
+                      </span>
+                            </td>
+                            <td>
+                                <div class="d-flex align-items-center">
+                                    {{$cart['created_at']}}
+{{--                                    <span class="completion mr-2">60%</span>--}}
+{{--                                    <div>--}}
+{{--                                        <div class="progress">--}}
+{{--                                            <div class="progress-bar bg-warning" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;"></div>--}}
+{{--                                        </div>--}}
+{{--                                    </div>--}}
+                                </div>
+                            </td>
+                            <td class="text-right">
+                                <div class="dropdown">
+                                    <a class="btn btn-sm btn-icon-only text-light" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        <i class="fas fa-ellipsis-v"></i>
+                                    </a>
+                                    <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
+                                        <a class="dropdown-item" href="{{route('carts.update',['cart'=>$cart['id']])}}">View Detail</a>
+                                        <a class="dropdown-item" href="{{route('carts.update',['cart'=>$cart['id']])}}">Edit</a>
+                                        <form method="POST" action="{{route('carts.destroy',['cart'=>$cart['id']])}}">
+                                            @csrf
+                                            @method('delete')
+                                            <button class="dropdown-item" type="submit">Delete</button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+            <!-- Card footer -->
+            <div class="card-footer py-4">
+                <nav aria-label="...">
+                    <ul class="pagination justify-content-end mb-0">
+                        <li class="page-item disabled">
+                            <a class="page-link" href="#" tabindex="-1">
+                                <i class="fas fa-angle-left"></i>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                        </li>
+                        <li class="page-item active">
+                            <a class="page-link" href="#">1</a>
+                        </li>
+                        <li class="page-item">
+                            <a class="page-link" href="#">2 <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="page-item"><a class="page-link" href="#">3</a></li>
+                        <li class="page-item">
+                            <a class="page-link" href="#">
+                                <i class="fas fa-angle-right"></i>
+                                <span class="sr-only">Next</span>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+</div>
