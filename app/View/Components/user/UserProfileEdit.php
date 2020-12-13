d1d28e635d0e45f4913670c2a67e10518a71b430
truonghoangduy <truonghoangduy03111999@gmail.com>
2020-12-13 19:41:55 +0700

    nothing change

---
+++ app/View/Components/user/UserProfileEdit.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\View\Components\user;
+
+use Illuminate\View\Component;
+
+class UserProfileEdit extends Component
+{
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public $userDetail;
+    public function __construct($userDetail)
+    {
+        $this->userDetail= $userDetail;
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|string
+     */
+    public function render()
+    {
+        return view('components.user.user-profile-edit');
+    }
+}
